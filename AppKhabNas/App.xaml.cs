fd5e251ba109b4893c3bf557895417bce02ee0a4
olbenko <albinaaaaa22@mail.ru>
2025-01-20 09:56:21 +0300

    forst commit

---
+++ AppKhabNas/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppKhabNas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
